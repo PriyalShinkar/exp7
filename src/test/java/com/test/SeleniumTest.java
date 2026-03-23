380f0cad96327906d7b4150c8b60b6376622b91a
PriyalShinkar <priyalshinkar23@gmail.com>
2026-03-23 14:18:08 +0530

    Add SeleniumTest class for browser testing
    Set up Selenium WebDriver for Chrome and Firefox with headless options based on Jenkins parameters.

---
+++ src/test/java/com/test/SeleniumTest.java
@@ -0,0 +1,44 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+public class SeleniumTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        // These capture the parameters you set in Jenkins 
+        String browser = System.getProperty("browser"); 
+        String url = System.getProperty("url");         
+
+        if (browser != null && browser.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless"); // Needed for Jenkins servers
+            driver = new ChromeDriver(options);
+        } 
+        else if (browser != null && browser.equalsIgnoreCase("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
+        }
+
+        if (url != null) {
+            driver.get(url); [cite: 40]
+        }
+    }
+
+    @Test
+    public void executeTest() {
+        System.out.println("Page Title is: " + driver.getTitle());
+        // Add your Selenium validation logic here 
+    }
+}
